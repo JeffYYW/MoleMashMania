68d9349cfeaffb1c4a31f0ba5b8c5280d477b6e4
Jeffrey Yeung <jeffyyw@gmail.com>
2019-05-20 13:59:46 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// create function that will iterate through an array (block) that will hold a list of divs that represent the moles
-// iterate through the array
-// for each div, call the raise() function on it
-// raise() will translateY this div upwards by 200px
-// update active property to 'true'
-// when the user clicks on a div with an active property of 'true', add 1 to the score
-// call lower() after a random setTimeout   
-// lower() will translateY this div back to its original position
-// update active property to false
-// reset counter and loop through the initial function again
-// setInterval for the initial function
 
-// const topDiv = document.getElementsByClassName('scoreContainer')[0];
+                // REFERENCES
+// reference used for the random function
+    // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals#Active_learning_a_reaction_game 
 
-const blockObj = [
+// reference used for setting a delay in a loop
+    // https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
+
+// timer created with assistance from David Thavixay
+
+
+const moleObj = [
     { active: false },
     { active: false },
     { active: false },
@@ -22,89 +19,68 @@ const blockObj = [
 ]
 
 const moles = document.getElementsByClassName('mole-div');
-// const items = document.getElementsByClassName('item-set');
-// const plus1 = document.getElementById("plusOne");
-
-// reference used for the random function
-// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals#Active_learning_a_reaction_game 
-// #3
-function random(min, max) {
-    let num = Math.floor(Math.random() * (max - min)) + min;
-    return num;
-}
-
 let score = 0;
-
-function lower(i) {
-    moles[i].style.transform = "translateY(0%)";
-    moles[i].style.height = "117%";
-    blockObj[i].active = false;
-    moles[i].style.visibility = 'hidden';
-    
-    console.log(blockObj[i]);
-}
-
-// let element = document.getElementsByClassName('score-animation'); 
-
-// add event listener: if clicked and active === true, call lower(). else setTimeout and call lower()
-
-// change these to add class (classList.add)
-
-function raise(i) {
-    moles[i].style.visibility = 'visible';
-    moles[i].style.transform = "translateY(-65%)";
-    moles[i].style.height = "145%";
-    blockObj[i].active = true;
-    moles[i].addEventListener("click", function () {
-        if (blockObj[i].active === true) {
-            moles[i].querySelector("img").src = "assets/diglett-hit-nodirt.png"
-            // element[i].classList.remove("score-animation");
-            // void element.offsetWidth;
-            $('#plus-one').addClass("score-animation");
-            score = score + 1;
-            $('#score').html(score);
-            lower(i);
-        }
-        
-    })
-    console.log(blockObj[i]);
-    setTimeout(lower, random(1000, 1200), i);
-}
-
 let seconds = undefined;
+let countdown = undefined;
+let quit = false;
 
 function timer() {
     let time = Date.now() + 32000;
-    return setInterval(function() {
+    return setInterval(function () {
         seconds = parseInt((time - Date.now()) / 1000)
         $('#timer').html(`${seconds}`);
     }, 1000)
 }
 
-// document.getElementById('plusOne').classList.add('animated', 'fadeOutUp');
 
-let countdown = undefined;
-// timer created with assistance from David Thavixay
+function random(min, max) {
+    let num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
 
-// reference used for setting a delay in a loop
-// https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
+// This function will set the mole back to its initial position behind the mound of dirt and set its 'active' state to 'false', making it unclickble for a point.
 
+function lower(i) {
+    moles[i].style.transform = "translateY(0%)";
+    moles[i].style.height = "117%";
+    moleObj[i].active = false;
+    moles[i].style.visibility = 'hidden';
+}
+
+// This function will first raise the mole 'div' upwards, add styling, and set its 'active 'state to 'true'. If the raised mole is clicked while 'active', the mole's image will change and 1 point will be added to the score. The lower() function is then called if the mole is clicked, or after a set timeout has elapsed. 
+
+function raise(i) {
+    moles[i].style.visibility = 'visible';
+    moles[i].style.transform = "translateY(-65%)";
+    moles[i].style.height = "145%";
+    moleObj[i].active = true;
+    moles[i].addEventListener("click", function () {
+        if (moleObj[i].active === true) {
+            moles[i].querySelector("img").src = "assets/diglett-hit-nodirt.png"
+            $('#plus-one').addClass("score-animation");
+            score = score + 1;
+            $('#score').html(score);
+            lower(i);
+        }
+    })
+    setTimeout(lower, random(1000, 1200), i);
+}
 
 
 $(function () {
+
+    // init() will first generate a random number between 0-5. The loop with the counter 'j' is resetting the images back to it's original state before the next mole is called. A setTimeout is used to 'pause' time between moles being raised. The function calls itself again with a new random number out of the array.
     function init() {
-        let i = random(0,6);                 // i is set to a random value between 0 and 6
-        // reset all 
+        let i = random(0,6);                
         for (j = 0; j < moles.length; j++) {
             moles[j].firstElementChild.src = "assets/diglett-no-dirt.png";
         }
         $('#plus-one').removeClass("score-animation");
-        setTimeout(function () {             // set timeout to pause time between blocks being raised
-            raise(i);                        // call raise() on i which is a random value between 0 and 6
+        setTimeout(function () {            
+            raise(i);                        
             if (quit === true) {
                 alert("Game stopped! Press start to play again!");
                 return
-                
             } else if ( seconds <= 0) {
                 alert("Time up!");
                 clearInterval(countdown);
@@ -112,11 +88,12 @@ $(function () {
                 $('.quit-button').css('display', 'none');
                 return
             } else {                              
-                init()                      // function calls itself again with a new random number out of the array
+                init()                      
             }
         }, random(1000, 1500))
     }
   
+    // Start Button
     $('.start-button').on("click", function () {
         quit = false;
         score = 0;
@@ -128,18 +105,17 @@ $(function () {
     })
 
 
-
-    quit = false;
+    // Quit Button
+    
     $('.quit-button').on("click", function () {
         quit = true;
         clearInterval(countdown);
         $('.start-button').css('display', 'block');
         $('.quit-button').css('display', 'none');
         return
-
     })
 
-
+    // Title Overlay
     $('.container').mousedown(function() {
         $('.container').css('cursor', 'url(assets/mallet-icon-down.png),auto');
     }).mouseup(function() {
